fcad4e95e78275245a7f7cb64b17513765a0d1e5
Rishabh <rishabhkanojiya75@gmail.com>
2020-05-17 17:31:26 +0530

    remove some files

--- src/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID:
-    "132779947063-cqj0ssiaj7oa020qk72dtm7j1oltbioq.apps.googleusercontent.com",
-  ClientSecret: "7TGNlWueaX-1bnpo1WU_QFeh",
-  cookieKey: "asdfghjklasldkmaskdalskpiajepsjc"
-};
